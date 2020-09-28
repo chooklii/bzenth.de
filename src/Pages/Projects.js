2c62ae8e6b3c1d2d86111f5a92411902439d7691
Chooklii <=benjaminzenth@icloud.com>
2020-09-28 18:49:01 +0200

    add Projects page and meta keywords

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -3,11 +3,93 @@ import {Header} from "../Components"
 
 class Projects extends React.Component {
 
+    oepnv_project(){
+        return(
+            <div className="projects-picture-left">
+
+            <div className="project-oepnv-image"></div>
+            <div className="project-left-content">
+                <div className="project-heading">ÖPNV-Transparenzregister</div>
+
+                <div className="project-description-one">
+                    Im Rahmen des Studiums im 7. Semester in einem Gruppenprojekt entscheidend
+                    weiterentwickelt und zum 12.11.2019 im Rahmen einer Pressekonferenz veröffentlicht.
+
+                    Die Seite kann von der Bevölkerung genutzt werden, um an einer zentralen Stelle
+                    z.B. gefahrene Buskilometer oder damit verbundene Kosten abzufragen
+                </div>
+                <div className="project-description-two">
+                    Presseartikel und weitere Informationen zum Projekt:
+                    <a className="project-press-link" target="_blank" href="https://background.tagesspiegel.de/mobilitaet/oepnv-transparenzregister-geht-online">Background Tagesspiegel</a>
+                    <a className="project-press-link" target="_blank" href="https://www.busnetz.de/oepnv-transparenzregister-geht-online/">Busnetz</a>
+                </div>
+
+                <div className="project-description-three">
+                    Personliches Aufgabengebiet: Komplettes Backend, welches die Datenabfragen auswertet und die entsprechenden Daten liefert.
+                </div>
+
+                <div className="project-link">
+                <a target="_blank" href="https://www.oepnv-transparenzregister.de/" className="project-link">Link zur Internetseite</a>
+                </div>
+            </div>
+
+        </div>
+        )
+    }
+
+    hartwork_project(){
+        return(
+            <div className="projects-picture-right">
+            <div className="project-right-content">
+            <div className="project-heading">#HARTWORK</div>
+            <div className="project-description-one">
+                    Coming soon
+                </div>
+            </div>
+            <div className="project-hartwork-image"></div>
+            </div>
+        )
+    }
+
+    bzenth_project(){
+        return(
+            <div className="projects-picture-left">
+
+            <div className="project-bzenth-image"></div>
+            <div className="project-left-content">
+                <div className="project-heading">Diese Internetseite</div>
+
+                <div className="project-description-one">
+                        Beschreibung
+                </div>
+
+            </div>
+
+        </div>
+        )
+    }
 
     render(){
+
         return(
             <div>
                 <Header/>
+
+                <div className="projects-box">
+                    <div className="projects-heading">
+                        Referenzen:
+                    </div>
+                    <div className="projects-heading-underline">
+                        Auswahl bisher realisierter Projekte
+                    </div>
+
+                    <div className="projects">
+                    {this.oepnv_project()}
+                    {this.hartwork_project()}
+                    {this.bzenth_project()}
+                    </div>
+
+                </div>
             </div>
         )
     }
