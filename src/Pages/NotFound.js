b8b1071413c978f61e51a5cb4ab79203fdd0a887
Benjamin Zenth <benjaminzenth@icloud.com>
2021-03-08 20:31:38 +0100

    remove some stuff from css

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Header} from "../Components"
+import {Footer, Header} from "../Components"
 
 class NotFound extends React.Component {
 
@@ -8,11 +8,12 @@ class NotFound extends React.Component {
         return(
             <div>
                 <Header/>
-                <div className="notFound-content">
+                <div className="page_classic">
                     Ich kann Ihnen bei ihrer eigenen Internetseite oder ihrem IT-Projekt helfen, aber diese Seite finden kann ich leider nicht.
                     Am besten nutzen Sie die Links im Header um auf eine gültige Adresse zu gelangen.
                 </div>
                 <div className="notFound-image"></div>
+                <Footer/>
             </div>
         )
     }
