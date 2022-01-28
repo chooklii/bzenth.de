4b28ec2100eae2e380a47de9fa793ab33a7376e9
Benjamin Zenth <benjaminzenth@icloud.com>
2022-01-28 10:55:42 +0100

    add job

--- src/Pages/AboutMe/content.js
+++ src/Pages/AboutMe/content.js
@@ -5,9 +5,10 @@ const cv_content = [
     {time: "09/2016 - 03/2020", text: "Studium Wirtschaftsinformatik"},
     {time: "09/2018 - 12/2019", text: "Werksstudent Anwendungsentwickler Bechtle AG"},
     {time: "12/2019 - 03/2020", text: "Bachelorthesis: Selektion und Einführung einer Front-End Error Reporting Anwendung - bei: Bechtle AG"},
-    {time: "04/2020 - 09/2020", text: "Vollzeit Anwendungsentwickler Bechtle AG"},
-    {time: "09/2020 - heute", text: "Master Wirtschaftsinformatik - Informationsmanagement und Data Science"},
-    {time: "09/2021 - heute", text: "Masterarbeit: Modelling and Simulating the scalability of web application architectures"}
+    {time: "04/2020 - 09/2020", text: "Vollzeit Softwareingenieur Bechtle AG"},
+    {time: "09/2020 - 03/2022", text: "Master Wirtschaftsinformatik - Informationsmanagement und Data Science"},
+    {time: "09/2021 - 03/2022", text: "Masterarbeit: Modelling and Simulating the scalability of web application architectures"},
+    {time: "04/2022 - ", text: "Softwareingenieur Bechtle AG"},
 ]
 
 const description = "Ich bin ein 24-jähriger aus dem Raum Öhringen und aktuell im 3. Semester meines Masterstudiums an der Hochschule Heilbronn. Zuvor habe ich an der HHN bereits meinen Bachelor abgeschlossen. In diesem Studium hatte ich meine ersten Kontakte mit der Entwicklung, welche ich seitdem durch Praxissemester, Werkstudententätigkeit, Vollanstellung und private Projekte breitgefächert vertieft habe."
