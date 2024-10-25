6bba56383526e58aa6bcd3819df022e62d30d414
Benjamin Zenth <benjaminzenth@icloud.com>
2024-10-25 10:55:32 +0200

    minimics css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const path = require("path");
 
 // add paths for subdomains here
@@ -59,6 +60,11 @@ module.exports = {
     output: {
       filename: '[name].js',
       path: path.resolve(__dirname, "docs")
+    },
+    optimization: {
+      minimizer: [
+        new CssMinimizerPlugin(),
+      ],
     },
     plugins: [
         new HtmlWebPackPlugin({
